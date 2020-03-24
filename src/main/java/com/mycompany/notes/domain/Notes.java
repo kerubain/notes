85354db52a81317b742803126f68d1caba29b545
angel.estrada <aerobles76@hotmail.com>
2020-03-24 12:16:19 -0700

    Add Notes entity

--- src/main/java/com/mycompany/notes/domain/Notes.java
+++ src/main/java/com/mycompany/notes/domain/Notes.java
@@ -1,10 +1,86 @@
 package com.mycompany.notes.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 public class Notes {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idNote;
+
+    @Column
+    private String username;
+
+    @Column
+    private String title;
+
+    @Column
+    private String note;
+
+    @Column
+    private Date createTime;
+
+    @Column
+    private Date updateTime;
+
+    private Notes(){}
+
+    public Notes(Long idNote, String username, String title, String note,Date createTime, Date updateTime){
+        this.idNote = idNote;
+        this.username = username;
+        this.title = title;
+        this.note = note;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public void setIdNote(Long idNote) {
+        this.idNote = idNote;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getIdNote() {
+        return idNote;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
 }
