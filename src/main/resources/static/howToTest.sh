e38d268ce159e67019cbfbe41f032d3ff9b91f96
angel.estrada <aerobles76@hotmail.com>
2020-03-24 12:57:35 -0700

    Test security

--- src/main/resources/static/howToTest.sh
+++ src/main/resources/static/howToTest.sh
@@ -1,6 +1,4 @@
 #how to test
 #Windows
-curl -H "Content-Type: application/json" -X POST --data "{\"username\":\"user1@domain.com\",\"title\":\"mydog\",\"note\":\"hansom\"}" http://localhost:8080/api/v1/notes
+curl --user user1@domain.com:123 -H "Content-Type: application/json" -X POST --data "{\"username\":\"user1@domain.com\",\"title\":\"mydog3\",\"note\":\"hansom3\"}" http://localhost:8080/api/v1/notes
 
-#later
-curl --user user1@domain.com:123 -H "Content-Type: application/json" -X POST --data "{\"username\":\"user1@domain.com\",\"title\":\"jton9\",\"note\":\"guapo9\"}" http://localhost:8080/api/v1/notes
