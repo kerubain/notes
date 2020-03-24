4d8aef22c498a5594a102f9259ae1d5654ed6c2c
angel.estrada <aerobles76@hotmail.com>
2020-03-24 12:48:12 -0700

    Testing insert

--- src/main/resources/static/howToTest.sh
+++ src/main/resources/static/howToTest.sh
@@ -1,3 +1,6 @@
 #how to test
 #Windows
-curl --user user1@domain.com:123 -H "Content-Type: application/json" -X POST --data "{\"title\":\"jton9\",\"note\":\"guapo9\"}" http://localhost:8080/api/v1/notes
+curl -H "Content-Type: application/json" -X POST --data "{\"username\":\"user1@domain.com\",\"title\":\"mydog\",\"note\":\"hansom\"}" http://localhost:8080/api/v1/notes
+
+#later
+curl --user user1@domain.com:123 -H "Content-Type: application/json" -X POST --data "{\"username\":\"user1@domain.com\",\"title\":\"jton9\",\"note\":\"guapo9\"}" http://localhost:8080/api/v1/notes
