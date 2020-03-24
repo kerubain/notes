3ea7c6a481ee3ef871a3a16d4b73bbb679dd9cd0
angel.estrada <aerobles76@hotmail.com>
2020-03-24 12:37:48 -0700

    Add RestController

--- src/main/java/com/mycompany/notes/controller/ResourceController.java
+++ src/main/java/com/mycompany/notes/controller/ResourceController.java
@@ -1,10 +1,45 @@
 package com.mycompany.notes.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mycompany.notes.domain.Notes;
+import com.mycompany.notes.service.NotesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ResourceController {
+    @Autowired
+    private NotesService notesService;
+
+    @RequestMapping(value = "/note", method = RequestMethod.GET)
+    public List<Notes> getNotesByUsername(@RequestParam String username){
+        return notesService.getNotesByUsername(username);
+    }
+
+    @RequestMapping(value = "/notes", method = RequestMethod.GET)
+    public Notes getNote(@RequestParam Long idNote){
+        return notesService.getNote(idNote);
+    }
+
+    @RequestMapping(value = "/note", method = RequestMethod.POST)
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Long insertNotes(@RequestBody Notes note){
+        return notesService.insertNotes(note);
+    }
+
+    @RequestMapping(value = "/note", method = RequestMethod.PUT)
+    @ResponseStatus(code = HttpStatus.OK)
+    public Notes updateNotes(@RequestParam Notes note){
+        return notesService.updateNotes(note);
+    }
+
+    @RequestMapping(value = "/note", method = RequestMethod.GET)
+    @ResponseStatus(code = HttpStatus.OK)
+    public void deleteNotes(@RequestParam Long idNote){
+        notesService.deleteNotes(idNote);
+    }
 
 }
